6b7c2855dfc1f0eb1e624ec28badd97298875523
yeondub <114345405+yeondub1121@users.noreply.github.com>
2024-05-07 21:39:26 +0900

    Update Movie.js

--- Movie/components/Movie.js
+++ Movie/components/Movie.js
@@ -5,24 +5,24 @@ export const API_URL = 'https://api.themoviedb.org/3/movie/';
 
 //nowPlayingList api
 export const getNowPlayingList = async () => {
-  const response = await axios.get(`${API_URL}/now_playing?api_key=${API_KEY}&language=en-KO&page=1`);
+  const response = await axios.get(`${API_URL}/now_playing?api_key=${API_KEY}&language=en-US&page=1`);
   return response.data;
 };
 
 //popularList api
 export const getPopularList = async () => {
-  const response = await axios.get(`${API_URL}/popular?api_key=${API_KEY}&language=en-KO&page=1`);
+  const response = await axios.get(`${API_URL}/popular?api_key=${API_KEY}&language=en-US&page=1`);
   return response.data;
 };
 
 //topRatedList api
 export const getTopRatedList = async () => {
-  const response = await axios.get(`${API_URL}/top_rated?api_key=${API_KEY}&language=en-KO&page=1`);
+  const response = await axios.get(`${API_URL}/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
   return response.data;
 };
 
 //upComingList api
 export const getUpComingList = async () => {
-  const response = await axios.get(`${API_URL}/upcoming?api_key=${API_KEY}&language=en-KO&page=1`);
+  const response = await axios.get(`${API_URL}/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
   return response.data;
 };
